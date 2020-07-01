85d89c2e299337f8a6615c7d24dd831cf51764e6
haritha <harithapnair2205@gmail.com>
2020-06-30 21:24:50 -0400

    updated package.json, procfile, and hello.js PORT

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
@@ -24,7 +25,7 @@ http.createServer(function (req, res) {
         return res.end();
     });
 
-}).listen(3050);
+}).listen(PORT);
     
     
 console.log("Server Started....");
